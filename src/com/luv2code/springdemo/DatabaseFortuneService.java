f4e799b333cd1c54246b13f92478e58ed7fb20a0
Patryk Glegoła <patryk.glegola@gmail.com>
2018-02-27 21:21:22 +0100

    Qualifiers for Dependency Injection lesson.

---
+++ src/com/luv2code/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
---
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+    // create an array of strings
+    private String[] data = {
+            "Beware of the wolf in sheep's clothing",
+            "Diligence is the mother of good luck",
+            "The journey is the reward"
+    };
+
+    //create a random number generator
+    private Random randomNumberGenerator = new Random();
+
+    @Override
+    public String getFortune() {
+        //pick a random string from the array
+        int index = randomNumberGenerator.nextInt(data.length);
+        String theFortune = data[index];
+        return theFortune;
+    }
+}
