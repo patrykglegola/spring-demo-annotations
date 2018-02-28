ade0231ab081636d03c52a6a9a7769f247758c59
Patryk Glegoła <patryk.glegola@gmail.com>
2018-02-28 01:55:35 +0100

    @Scope annotation lesson

---
+++ src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,24 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+
+        System.out.println("Pointing to the same object? " + result);
+        System.out.println("Memory location for theCoach" + theCoach);
+        System.out.println("Memory location for alphaCoach" + alphaCoach);
+
+        context.close();
+
+    }
+}
