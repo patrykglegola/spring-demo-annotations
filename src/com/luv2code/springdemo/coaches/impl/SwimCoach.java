7dca357b97e090048fe2f9aba1820f20a241c40d
Patryk Glegoła <patryk.glegola@gmail.com>
2018-02-28 14:32:14 +0100

    Defining Spring beans with Java code lesson.

--- src/com/luv2code/springdemo/coaches/impl/SwimCoach.java
+++ src/com/luv2code/springdemo/coaches/impl/SwimCoach.java
@@ -2,20 +2,18 @@ package com.luv2code.springdemo.coaches.impl;
 
 import com.luv2code.springdemo.coaches.Coach;
 import com.luv2code.springdemo.services.FortuneService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
-@Component
-public class FootballCoach implements Coach {
+public class SwimCoach implements Coach {
 
-    @Autowired
-    @Qualifier("propertiesFileFortuneService")
     private FortuneService fortuneService;
+
+    public SwimCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
-        return "Train passing the ball for 30 minutes.";
+        return "Swim 1000 metres as a warm up.";
     }
 
     @Override
--- src/com/luv2code/springdemo/config/SportConfig.java
+++ src/com/luv2code/springdemo/config/SportConfig.java
@@ -1,10 +1,27 @@
 package com.luv2code.springdemo.config;
 
+import com.luv2code.springdemo.coaches.Coach;
+import com.luv2code.springdemo.coaches.impl.SwimCoach;
+import com.luv2code.springdemo.services.FortuneService;
+import com.luv2code.springdemo.services.impl.SadFortuneService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.luv2code.springdemo")
+//@ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 
+    // define bean for sad fortune service
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    // define bean for swim couch and inject dependency
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
+
